3d579e3c30b18b5b894ed25807e5b1786178eff9
程飞 <fay.cheng.cn@gmail.com>
2017-09-11 15:41:19 +0800

    add test cases for random enum

--- tests/test_faker.py
+++ tests/test_faker.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
 
+import random
 import string
+import pytest
+
 from candy_utils import faker
 
 
@@ -32,10 +35,33 @@ def test_random_number_str():
 
 def test_random_str():
     s = faker.random_string()
-    assert s in string.ascii_letters + string.digits
+    for c in s:
+        assert c in list(string.ascii_letters + string.digits)
     assert len(s) == 8
 
     s = faker.random_string(length=10)
     assert len(s) == 10
+
+
+def test_random_enum():
+    enums = ['RED', 'GREEN', 'YELLOW']
+    enum = faker.random_enum(enums)
+    assert enum in enums
+
+    def gen_enums():
+        for color in ['RED', 'GREEN', 'YELLOW']:
+            yield color
+
+    enums = gen_enums()
+    enum = faker.random_enum(enums)
+    assert enum in ['RED', 'GREEN', 'YELLOW']
+
+    for enums in [None, 123]:
+        with pytest.raises(TypeError):
+            faker.random_enum(enums)
+
+    for enums in [[], {}]:
+        with pytest.raises(IndexError):
+            faker.random_enum(enums)
 
 
