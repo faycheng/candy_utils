3c8aab787ea3fdb5d74157e68164136e53f1ddb1
程飞 <fay.cheng.cn@gmail.com>
2017-09-11 15:25:59 +0800

    add test case for random upper

--- tests/test_faker.py
+++ tests/test_faker.py
@@ -12,3 +12,12 @@ def test_random_lower_str():
     assert len(s) == 10
 
 
+def test_random_upper_str():
+    s = faker.random_lower_string()
+    assert s == s.upper()
+    assert len(s) == 8
+
+    s = faker.random_lower_string(length=10)
+    assert len(s) == 10
+
+
