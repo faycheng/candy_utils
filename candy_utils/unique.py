3caaa466197eb29d0f454ba0b3df3b4657349191
程飞 <fay.cheng.cn@gmail.com>
2017-09-12 23:02:39 +0800

    replace attr & callable by key_func

--- candy_utils/unique.py
+++ candy_utils/unique.py
@@ -4,35 +4,30 @@ import types
 from functools import wraps
 
 
-def _compare(item, another, attr, callable):
-    if attr is None:
+def _compare(item, another, key_func=None):
+    if key_func is None:
         return item == another
-    try:
-        if not callable:
-            return getattr(item, attr) == getattr(another, attr)
-        return getattr(item, attr)() == getattr(another, attr)()
-    except AttributeError:
-        return False
+    return key_func(item) == key_func(another)
 
 
-def _iter(obj, attr, callable):
-    obj = sorted(obj)
+def _iter(obj, key_func=None):
+    obj = sorted(obj, key=key_func)
     last = object()
     for item in obj:
-        if _compare(item, last, attr, callable):
+        if _compare(item, last, key_func) is True:
             continue
         yield item
         last = item
 
 
-def unique(attr=None, callable=False):
+def unique(key_func=None):
     def func_decorator(func):
         @wraps(func)
         def inner(*args, **kwargs):
             res = func(*args, **kwargs)
             if not (isinstance(res, list) or isinstance(res, types.GeneratorType)):
                 return res
-            return _iter(res, attr, callable)
+            return _iter(res, key_func)
 
         return inner
 
--- tests/test_unique.py
+++ tests/test_unique.py
@@ -12,7 +12,7 @@ def test_compare():
     item = faker.random_string()
     another = item
 
-    assert unique._compare(item, another, None, False) is True
+    assert unique._compare(item, another, None) is True
 
     class Item(object):
         pass
@@ -22,29 +22,13 @@ def test_compare():
     setattr(item, attr, attr_value)
     another = Item()
     setattr(another, attr, attr_value)
-    assert unique._compare(item, another, attr, False) is True
-
-    def item_compare_func():
-        return 'hello world'
-
-    def another_compare_func():
-        return 'hello world'
-
-    item = Item()
-    setattr(item, 'compare_func', item_compare_func)
-    another = Item()
-    setattr(another, 'compare_func', another_compare_func)
-    assert unique._compare(item, another, 'compare_func', True) is True
-
-    item = Item()
-    another = Item()
-    unique._compare(item, another, faker.random_string(), False) is False
+    assert unique._compare(item, another, lambda item: getattr(item, attr)) is True
 
 
 def test_iter():
     with mock.patch.object(unique, '_compare', return_value=False):
         l = [1, 2, 3]
-        res = unique._iter(l, None, False)
+        res = unique._iter(l, None)
         assert isinstance(res, types.GeneratorType)
         res = list(res)
         assert len(res) == 3
@@ -54,7 +38,7 @@ def test_iter():
 def test_unique():
     def func():
         return [1, 2, 3, 2]
-    res = unique.unique(None, False)(func)()
+    res = unique.unique(None)(func)()
     assert isinstance(res, types.GeneratorType)
     res = list(res)
     assert len(res) == 3
@@ -63,14 +47,9 @@ def test_unique():
     def func():
         for i in [1, 2, 3, 2]:
             yield i
-    res = unique.unique(None, False)(func)()
+    res = unique.unique(None)(func)()
     assert isinstance(res, types.GeneratorType)
     res = list(res)
     assert len(res) == 3
     assert set(res) == set([1, 2, 3])
-
-    def func():
-        return 'hello world'
-    res = unique.unique(None, False)(func)()
-    assert res == 'hello world'
 
